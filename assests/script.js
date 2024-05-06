a902571c9250b9b0feb6876861a95a68e30681dd
ndzamboni <ndzamboni@gmail.com>
2024-05-05 22:17:11 -0400

    setting up the framework

---
+++ assests/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // Function to fetch movie data from IMDb API
+    function fetchMovies() {
+        // Make API request to fetch movies
+        
+        // Process the response and create movie cards
+    }
+
+    // Function to create movie cards
+    function createMovieCard(movie) {
+        // Create HTML structure for movie card using movie details
+        // Add drag and drop attributes
+        // Append the card to the movieCards container
+    }
+
+    // Fetch movies when the page loads
+    fetchMovies();
+});
+
